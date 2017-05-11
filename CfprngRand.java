8d3afdb23b80a297a01b10c5947c4ee3bc2b9a4e
User <user@dyadic.pivotaldsm.com>
2017-05-11 18:49:41 -0400

    added hex pretty printing

--- CfprngRand.java
+++ CfprngRand.java
@@ -15,22 +15,51 @@ public class CfprngRand {
        return values
     */
 
+
     public static final int CFPRNG_SUCCESS = 0x00;
     public static final int CFPRNG_ERR = 0x01;
     //public native int cfprng_fips_rand(byte[] buf, int len);
     public native int cfprng_nist_rand(byte[] buf, int len);
     public native int cfprng_fips_tests();
-
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    
     static {
 
 	System.loadLibrary("cfprng_rand_jni");
     }
 
+    /* For debugging */
+
+    public static String bytesToHex(byte[] bytes) {
+	char[] hexChars = new char[bytes.length * 2];
+	for ( int j = 0; j < bytes.length; j++ ) {
+	    int v = bytes[j] & 0xFF;
+	    hexChars[j * 2] = hexArray[v >>> 4];
+	    hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	}
+	return new String(hexChars);
+    }
+
+    /* For debugging */
+
+    public static void printBytesInHex(byte[] buf) {
+
+	System.out.println(bytesToHex(buf));
+    }
+
     public static void main(String[] args) {
 
-
-
+	int keylen = 16;
+	byte[] key = new byte[keylen];
+	printBytesInHex(key);
 	CfprngRand rand = new CfprngRand();
+	rand.cfprng_nist_rand(key,keylen);
+	rand.cfprng_fips_tests();
+	printBytesInHex(key);
+	System.out.println("Loaded lib and ran tests");
     }
+
+
+    
 
 }
