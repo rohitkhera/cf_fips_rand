dea5c5c10ff1927c9cf8def11ef44ded880d02a0
Iryna Shustava <ishustava@pivotal.io>
2017-05-12 13:41:50 -0700

    populating hjava byte array from RAND_bytes() and passng back to java

--- cfprng_rand_java_wrapper.c
+++ cfprng_rand_java_wrapper.c
@@ -22,11 +22,22 @@ JNIEXPORT jint JNICALL Java_CfprngRand_cfprng_1fips_1rand
 JNIEXPORT jint JNICALL Java_CfprngRand_cfprng_1nist_1rand
 (JNIEnv * env, jobject obj, jbyteArray buf, jint len) {
 
-  if(cfprng_nist_rand((unsigned char*) buf, (int) len)) {
+  jbyte* bufferPtr = (*env)->GetByteArrayElements(env, buf, NULL);
+
+  jsize lengthOfArray = (*env)->GetArrayLength(env, buf);
+
+  
+  
+  if(lengthOfArray != len) return CFPRNG_ERR;
+  if(cfprng_nist_rand((unsigned char*) bufferPtr, (int) len)) {
     fprintf(stderr, "%s : ERROR\n", __FILE__);
     return (jint)CFPRNG_ERR;
   }
-  else return (jint)CFPRNG_SUCCESS;
+
+  else {
+    (*env)->ReleaseByteArrayElements(env, buf, bufferPtr, 0);
+    return (jint)CFPRNG_SUCCESS;
+  }
 }
 
 
